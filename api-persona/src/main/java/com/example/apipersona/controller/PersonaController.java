0c1b746ced895d25ddfce7acfdc44ea0b905e6d9
damiandamonte <damontedamian@gmail.com>
2023-05-22 18:01:00 -0300

    add gateway

--- api-persona/src/main/java/com/example/apipersona/controller/PersonaController.java
+++ api-persona/src/main/java/com/example/apipersona/controller/PersonaController.java
@@ -1,15 +1,7 @@
 package com.example.apipersona.controller;
 
 import com.example.apipersona.model.Persona;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
